eacac76794b8d7457674ab077828de85e964ec22
olangness <olson.langness@gmail.com>
2019-05-05 14:26:30 -0500

    added ng if to appointments and made new component

--- asdgroup/src/app/app.module.ts
+++ asdgroup/src/app/app.module.ts
@@ -3,26 +3,28 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import {AngularFireModule} from '@angular/fire';
-import {AngularFirestoreModule} from '@angular/fire/firestore';
-import {AngularFireStorageModule} from '@angular/fire/storage';
-import {AngularFireDatabaseModule} from 'angularfire2/database';
+//import {AngularFireModule} from '@angular/fire';
+//import {AngularFirestoreModule} from '@angular/fire/firestore';
+//import {AngularFireStorageModule} from '@angular/fire/storage';
+//import {AngularFireDatabaseModule} from 'angularfire2/database';
 import {environment} from '../environments/environment';
 import { AppointmentCardComponent } from './appointment-card/appointment-card.component';
+import { HomedetailsComponent } from './homedetails/homedetails.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     AppointmentCardComponent,
+    HomedetailsComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
-    AngularFireStorageModule,
-    AngularFireDatabaseModule
+//    AngularFireModule.initializeApp(environment.firebase),
+  //  AngularFirestoreModule,
+  //  AngularFireStorageModule,
+  //  AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
