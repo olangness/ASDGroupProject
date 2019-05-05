b6cc347399c5a0734df1751edc46de21f6af1b66
Matt Martin <matthew.martin47@gmail.com>
2019-05-05 13:55:50 -0500

    Update to user array

--- asdgroup/src/app/login/login.component.ts
+++ asdgroup/src/app/login/login.component.ts
@@ -16,27 +16,32 @@ export class LoginComponent implements OnInit {
     {
         username: 'mattmartin',
         password: '123',
-        role: 'student'
+        role: 'student',
+        studentID: 1
     },
     {
         username: 'jarrettmeester',
         password: '456',
-        role: 'advisor'
+        role: 'advisor',
+        advisorID: 1
     },
     {
         username: 'sampenney',
         password: '789',
-        role: 'student'
+        role: 'student',
+        studentID: 2
     },
     {
         username: 'drewwickoren',
         password: '123',
-        role: 'student'
+        role: 'student',
+        studentID: 3
     },
     {
         username: 'olsonlangness',
         password: '456',
-        role: 'advisor'
+        role: 'advisor',
+        advisorID: 2
     },
 ]
 
