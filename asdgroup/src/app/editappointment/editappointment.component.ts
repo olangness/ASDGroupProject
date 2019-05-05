c3b3e1db8b4a4f8de42d4e0af7dc714794a581cc
olangness <olson.langness@gmail.com>
2019-05-05 14:30:40 -0500

    added new components

---
+++ asdgroup/src/app/editappointment/editappointment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editappointment',
+  templateUrl: './editappointment.component.html',
+  styleUrls: ['./editappointment.component.css']
+})
+export class EditappointmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
