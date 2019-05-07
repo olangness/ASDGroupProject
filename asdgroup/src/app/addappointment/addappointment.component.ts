f2150c4a9c33f5fd2b88d9959be1a4249a07ea53
Matt Martin <49728198+MattMartin47@users.noreply.github.com>
2019-05-07 14:44:17 -0500

    comments

--- asdgroup/src/app/addappointment/addappointment.component.ts
+++ asdgroup/src/app/addappointment/addappointment.component.ts
@@ -18,24 +18,26 @@ export class AddappointmentComponent implements OnInit {
   }
 
   add() {
-    var  hour: string = (<HTMLSelectElement>document.getElementById('hourBox')).value;
+    var  hour: string = (<HTMLSelectElement>document.getElementById('hourBox')).value; //these get the values from the html elements and assigns them to variables to be used in making the appointment
     var  minute: string = (<HTMLSelectElement>document.getElementById('minuteBox')).value;
     var  amOrPM: string =(<HTMLSelectElement>document.getElementById('amOrPmBox')).value;
     var  reason: string = (document.getElementById("reason") as HTMLInputElement).value;
     var studentID: number = parseInt((document.getElementById("sID") as HTMLInputElement).value);
     var advisorID: number = parseInt((document.getElementById("aID") as HTMLInputElement).value);
     var appID: number = parseInt((document.getElementById("appID") as HTMLInputElement).value);
-    console.log("hour: " + hour);
+    var time: string = hour + ":" + minute + amOrPM;
+
+    console.log("hour: " + hour); //tests to make sure data is being read
     console.log("minute: " + minute);
     console.log("amOrPM: " + amOrPM);
     console.log("reason: " + reason);
     console.log("studentID: " + studentID);
     console.log("advisorID: " + advisorID);
     console.log("appID: " + appID);
-    var time: string = hour + ":" + minute + amOrPM;
+    
 
-    var newAppointment: Appointment = {advisorID: advisorID, studentID: studentID, appointmentReason: reason, appointmentID: appID, appointmentTime: time };
-    console.log("AdvisorID: " + newAppointment.advisorID);
+    var newAppointment: Appointment = {advisorID: advisorID, studentID: studentID, appointmentReason: reason, appointmentID: appID, appointmentTime: time }; //instantiates object of type appointment
+    console.log("AdvisorID: " + newAppointment.advisorID); //tests to make sure the object got the values it should have
     console.log("StudentID: " + newAppointment.studentID);
     console.log("AppointmentID: " + newAppointment.appointmentID);
     console.log("Reason: " + newAppointment.appointmentReason);
